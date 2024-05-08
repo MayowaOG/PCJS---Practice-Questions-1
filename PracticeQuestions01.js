20dd37cdaa40b90c438422dbf038b6e5c821be9c
MayowaOG <155774255+MayowaOG@users.noreply.github.com>
2024-05-08 20:50:53 -0230

    Add files via upload

---
+++ PracticeQuestions01.js
@@ -0,0 +1,121 @@
+// Project: Practice Questions 1
+// Created by: Mayowa Ogunyemi
+// Date created: May 6, 2024
+// Date completed: May 8, 2024 
+
+
+// Question 1 ==================================================================
+const label = "keyincollege";
+const ltd = "ca";
+const domainName = label + ltd;   console.log(domainName);
+console.log(``);
+
+// Question 2 ==================================================================
+isKeyin = label.toLowerCase().includes("keyin");
+console.log(isKeyin);
+console.log(``);
+
+// Question 3 ==================================================================
+isNotKeyin =! isKeyin;  console.log(isNotKeyin);
+console.log(``);
+
+// Question 4 ==================================================================
+let byte1 = 2;
+let byte2 = 4;
+let byte3 = 64;
+let byte4 = 128;
+
+const ipAddress = `${byte1}.${byte2}.${byte3}.${byte4}`;  console.log(ipAddress)
+console.log(``)
+
+// Question 5 ==================================================================
+multiNumber = 15
+for(var a = 1; a < 11; a++) {console.log(a * multiNumber)};
+console.log(``);
+
+// Question 6 ==================================================================
+const integers = [1,2,3,4,5,6,7,8,9,10];
+
+for(let i = 1; i <= 10; i ++) {
+    let integersNew = integers.map(multiplyArray);
+    function multiplyArray(num) {
+        return num * i
+        };
+    console.log(integersNew);
+};
+console.log(``);
+
+// Question 7 & 8 ================================================================
+let evenCounter = 0
+// let oddCounter = 0
+
+for(var i = 1; i <= 100; i+=1) {
+
+    if(i %2 == 0) {console.log(`${i} is an EVEN number`);
+        evenCounter += i}
+    else if(i % 2 == 1) {console.log(`${i} is an ODD number`)}    
+    };
+    console.log(``);
+    console.log(`Sum of even numbers: ${evenCounter}`)
+    console.log(``);
+
+
+
+// Question 9 ==================================================================
+function isPerfect(modulusNumber) {
+    let n = 0;
+    let sum = 0;
+
+    for(let i = 0; i < modulusNumber; i++) {
+        n += 1;
+        if(modulusNumber % n === 0) {
+            sum += n;
+        };
+    };
+
+    if(sum - modulusNumber === modulusNumber) {
+
+        console.log(`******* ${modulusNumber} is a PERFECT NUMBER *******`)
+    } else {console.log(`    ${modulusNumber} is a NOT a perfect number    `)}
+    
+    return modulusNumber
+};
+
+
+isPerfect(28);
+console.log(``);
+
+for(let i = 1; i <= 30; i++) {
+    isPerfect(i);
+};
+
+console.log(``);
+
+// Question 10 =================================================================
+function isPrime (primusNumber){ 
+    let n = 0;
+    let sum = 0;
+
+    for(let i = 0; i < primusNumber; i++) {
+        n += 1;
+        if(primusNumber % n === 0) {
+            sum += n
+        };
+    };
+
+    if(sum - primusNumber < 2) {
+        console.log(`**${primusNumber} is a PRIME NUMBER**`)}
+    else {
+        console.log(`${primusNumber} is NOT a prime number`)
+    };
+    return primusNumber
+}
+
+isPrime(47);
+isPrime(48);
+console.log(``);
+
+for(let i = 1; i <= 50; i++) {
+    isPrime(i);
+};
+
